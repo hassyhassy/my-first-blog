ff6132495bbb960187c2ef2963fdcc3205fc0e79
hassy <hassy.python@gmail.com>
2022-03-09 16:37:41 +0900

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  9 15:37:20 2022
+
+@author: kouki
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('',views.post_list,name='post_list'),
+]
